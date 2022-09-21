78b994991e5c847a3bfaa79ec969e0dca0d9e49d
keshav rana <keshavrana>
2022-09-21 22:12:43 +0530

    links added

--- src/http.js
+++ src/http.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 export default axios.create({
-    baseURL:"http://localhost:8000/api",
+    baseURL:"http://keshav01.herokuapp.com/api",
     headers:{
         "Content-type":"application/json"
     }
